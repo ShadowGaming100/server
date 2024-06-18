7151271d3ed64a829ed7d8a75b52fe26f94438f1
Shadow Gaming <81495140+ShadowGaming100@users.noreply.github.com>
2024-06-18 19:38:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
   const app = express()
-  const port = 8081 | 8082 | 8083
+  const port = 8084
   const path = require('path');
 
   app.get('/', (req, res) => {
